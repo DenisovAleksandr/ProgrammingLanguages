c516be52b9ef1cb7b3e68317bc5d0bb5ae820f15
Demisov <lyricistsucks@gmail.com>
2023-06-15 21:13:19 +0300

    Ex4

--- hw/hw1/Program.cs
+++ hw/hw1/Program.cs
@@ -1,6 +1,7 @@
-﻿/*
-Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, 
-какое число большее, а какое меньшее.
+﻿/*Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.*/
+
+/*
 Console.WriteLine("Input first numbers: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second numbers: ");
@@ -9,9 +10,10 @@ if (number1<number2) Console.WriteLine("max - "+number2+"; min - "+number1);
     else if (number2<number1) Console.WriteLine("max - "+number1+"; min - "+number2);
         else Console.WriteLine("the numbers are equal");
 */
+
 /*Задача 4: Напишите программу, которая принимает на вход три числа и 
 выдаёт максимальное из этих чисел.*/
-
+/*
 Console.WriteLine("Input first numbers: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second numbers: ");
@@ -31,3 +33,13 @@ if (number1 < number2){
     else
         Console.WriteLine("max - "+number1);
 }
+*/
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).*/
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 != 0)
+Console.WriteLine(number + " - odd number");
+else
+Console.WriteLine(number + " - even number");
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
