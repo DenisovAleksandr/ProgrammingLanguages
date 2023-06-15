315ee2abb6dddc6bb2cc47e6f1cc79128ddfa616
Demisov <lyricistsucks@gmail.com>
2023-06-15 20:55:49 +0300

    2exemple

--- hw/hw1/Program.cs
+++ hw/hw1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+Console.WriteLine("Input first numbers: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second numbers: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1<number2) Console.WriteLine("max - "+number2+"; min - "+number1);
+    else if (number2<number1) Console.WriteLine("max - "+number1+"; min - "+number2);
+        else Console.WriteLine("the numbers are equal");
+*/
+/*Задача 4: Напишите программу, которая принимает на вход три числа и 
+выдаёт максимальное из этих чисел.*/
+
+Console.WriteLine("Input first numbers: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second numbers: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third numbers: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
