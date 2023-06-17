a6f4100f72dc5a51753fda6f7921f0364195ae7e
Demisov <lyricistsucks@gmail.com>
2023-06-17 12:08:56 +0300

    ex15

--- hw/hw2/Program.cs
+++ hw/hw2/Program.cs
@@ -23,7 +23,7 @@ bool Check (int num) {
 */
 /*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
 сообщает, что третьей цифры нет.*/
-
+/*
 Console.WriteLine("Enter number");
 int num = Convert.ToInt32(Console.ReadLine()); 
 
@@ -44,7 +44,33 @@ if (!Check(num))
     Console.WriteLine("The third digit is missing");
 else
     Console.WriteLine("The third digit is "+ThirdDigit(num));
-
+*/
 
 /*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день
 недели, и проверяет, является ли этот день выходным.*/
+
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine()); 
+while (!Check(num))
+{
+    Console.WriteLine("Invalid mumber! Please, enter number [1;7]");
+    num = Convert.ToInt32(Console.ReadLine()); 
+}
+if (CheckWeekend(num))
+    Console.WriteLine($"{num} day of the weeks is Weekend ");
+    else
+    Console.WriteLine($"{num} day of the weeks is not Weekend ");
+
+bool Check(int num){
+    if ((num>0)&&(num<8))
+        return true;
+    else 
+        return false;
+}
+
+bool CheckWeekend (int num){
+    if (num<6)
+        return false;
+    else
+        return true;
+}
