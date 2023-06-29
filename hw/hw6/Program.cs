598199f63944f4c7908c401c0834dbacc0f54f58
Demisov <lyricistsucks@gmail.com>
2023-06-29 10:36:38 +0300

    ex1

--- hw/hw6/Program.cs
+++ hw/hw6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine($"Enter numbers separated by ',' ");
+int[] numbers = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+Console.WriteLine("Number of positive numbers - "+PositiveNumbersCount(numbers));
+
+int PositiveNumbersCount (int[] arr)
+{
+    int count = 0;
+    for (int i = 0;i < arr.Length ; i++)
+    {
+        if (arr[i] > 0)
+            count++;
+    }
+    return count;  
+}
+
