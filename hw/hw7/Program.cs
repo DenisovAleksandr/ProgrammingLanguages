56501873f9359baa10df5c96fb4567a3c66ea9d9
Demisov <lyricistsucks@gmail.com>
2023-07-09 19:29:47 +0300

    ex1

--- hw/hw7/Program.cs
+++ hw/hw7/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] GenerateArray(int arr_rows, int arr_cols)
+{
+    Random rand = new Random();
+    double[,] arr = new double[arr_rows, arr_cols];
+    for (int i = 0;i < arr_rows; i++)
+        for (int j = 0;j<arr_cols;j++)
+        arr[i,j] =Math.Round((MIN + rand.NextDouble()*(MAX-MIN)),2);
+    return arr;
+}
+void PrintArray (double[,] arr)
+{
+    Console.Write("[");
+    for (int i = 0;i < arr.length(1) ; i++)
+        for(int j = 0; j < arr.length(0); j++)
+        Console.Write(arr[i]+", ");
+    Console.WriteLine("\b\b]");
+}
